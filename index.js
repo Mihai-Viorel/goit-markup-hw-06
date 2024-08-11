cc7c7e6ceffb29fcce22b8bb371b2ffd83a511b0
MihaiViorel <mike.vios.125@gmail.com>
2024-08-11 17:04:25 +0300

    Website Responsive 1

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// modal hero button
+
 const button = document.querySelector(".btn-modal");
 const backdrop = document.querySelector(".modal-backdrop");
 
@@ -15,3 +17,24 @@ backdrop.addEventListener("click", (e) => {
 function toggleModal() {
   backdrop.classList.toggle("is-open");
 }
+
+// burger menu mobile
+
+const burgerButton = document.querySelector(".btn-burger-modal");
+const burgerBackdrop = document.querySelector(".burger_menu");
+
+burgerButton.addEventListener("click", toggleBurgerModal);
+
+burgerBackdrop.addEventListener("click", (e) => {
+  const target = e.target.closest(".burger_menu_btn-close-modal");
+
+  if (!target) {
+    return;
+  }
+  toggleBurgerModal();
+});
+
+function toggleBurgerModal() {
+  burgerBackdrop.classList.toggle("is-burger-open");
+}
+
